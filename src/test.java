abf319dd20f54667aa34e2e3dd112268d67a3abd
Greed-A <kalatilin.anton@yandex.ru>
2019-03-07 09:02:29 +0400

    test classes

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String[] args) {
         int i;
         double s, z;
-        i = 4;
+        i = 3;
         s = 5.5;
         z = s / i;
         System.out.println("i = "+i);
