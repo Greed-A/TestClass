d18f34096359b94f8e31a8160aa1f054c72b48f3
Greed-A <kalatilin.anton@yandex.ru>
2019-03-07 09:01:55 +0400

    test classes

--- src/test.java
+++ src/test.java
@@ -1,6 +1,12 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("master");
-        System.out.println("t");
+        int i;
+        double s, z;
+        i = 4;
+        s = 5.5;
+        z = s / i;
+        System.out.println("i = "+i);
+        System.out.println("s = " + s);
+        System.out.printf("z = %f",z);
     }
 }
